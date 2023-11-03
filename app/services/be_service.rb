3b9d46e431259249a81f5a2480340534712e474b
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-11-03 01:01:57 -0600

    comment out code

--- app/services/be_service.rb
+++ app/services/be_service.rb
@@ -1,24 +1,24 @@
-class BEService
-  def self.conn
-    Faraday.new('http://localhost:3000/')
-  end
+# class BEService
+#   def self.conn
+#     Faraday.new('http://localhost:3000/')
+#   end
 
-  def self.login_user(user_input)
-    response = conn.post("/api/v1/customers") do |req|
-      req.headers[:CONTENT_TYPE] = "application/json"
-      req.body = JSON.generate(user: user_input)
-    end
-    a = JSON.parse(response.body, symbolize_names: true)
-  end
+#   def self.login_user(user_input)
+#     response = conn.post("/api/v1/customers") do |req|
+#       req.headers[:CONTENT_TYPE] = "application/json"
+#       req.body = JSON.generate(user: user_input)
+#     end
+#     a = JSON.parse(response.body, symbolize_names: true)
+#   end
 
-  def self.teas
-    response = conn.get("/api/v1/teas") 
-    JSON.parse(response.body, symbolize_names: true)
-  end
+#   def self.teas
+#     response = conn.get("/api/v1/teas") 
+#     JSON.parse(response.body, symbolize_names: true)
+#   end
 
-  def self.find_tea(tea_id)
-    response = conn.get("/api/v1/teas/#{tea_id}") 
-    JSON.parse(response.body, symbolize_names: true)
+#   def self.find_tea(tea_id)
+#     response = conn.get("/api/v1/teas/#{tea_id}") 
+#     JSON.parse(response.body, symbolize_names: true)
 
-  end
-end
+#   end
+# end
