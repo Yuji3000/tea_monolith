af26fae0de985e4e7d1d671787442006c6580581
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-11-10 14:52:55 -0700

    change app name

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-module TeaTimeFe
+module TeaTime
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
