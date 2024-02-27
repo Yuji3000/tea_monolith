026b2ba2287d8652ddcf656c6ba05079528c4099
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2024-02-27 11:33:29 -0700

    Add to readme

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 require 'webmock/rspec'
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# Rails.application.load_seed
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
